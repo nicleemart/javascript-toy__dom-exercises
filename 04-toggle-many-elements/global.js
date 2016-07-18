6edb8c4b818c768ee242adf51bcfae3f7ba5e78c
Nikki <nicleemart@gmail.com>
2016-07-18 18:11:32 -0500

    Completed exercise 04

--- 04-toggle-many-elements/global.js
+++ 04-toggle-many-elements/global.js
@@ -1,1 +1,20 @@
-// Your JavaScript goes here.
+window.addEventListener("load", function(){
+
+	var button = document.getElementById("button");
+
+	button.addEventListener("click", function(){
+
+		var toggle_divs = document.getElementsByClassName("toggle_me");
+
+		for (var i = 0; i < toggle_divs.length; i++){			
+			if (toggle_divs[i].style.display === "none"){
+				toggle_divs[i].style.display = "block";
+			}
+
+			else {
+				toggle_divs[i].style.display = "none";
+			}
+		}
+	});
+
+});
