abaf8586f01f7dfe3b30955bb4e7b9a304938b76
Nikki <nicleemart@gmail.com>
2016-07-18 19:41:01 -0500

    Completed exercise 06

--- 06-detecting-typed-input/global.js
+++ 06-detecting-typed-input/global.js
@@ -1,1 +1,13 @@
-// Your JavaScript goes here.
+window.addEventListener("load", function(){
+
+	var input_name = document.getElementById("full_name");
+
+	input_name.addEventListener("keyup", function(){
+
+		var greeting = document.getElementById("greeting");
+
+		greeting.innerHTML = ("Hello there, " + input_name.value + "!");
+
+	});
+
+});
